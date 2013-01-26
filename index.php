b4eca18077377f119bb34b67ebf7babcf8ade728
roccosportal <rocco@roccosportal.com>
2013-01-26 13:15:42 +0100

    fix for new version

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 
     error_reporting(E_ALL ^ E_NOTICE);
     
-    require_once ("./library/pvik/core/core.php");
-    $Core = new Pvik\Core\Core();
-    $Core->Init()
-        ->LoadConfig()
+    // set up path mapper
+    require './library/pvik/Core/Path.php';
+    \Pvik\Core\Path::Init();
+    
+    // set up class loader
+    require \Pvik\Core\Path::RealPath('~/library/pvik/Core/ClassLoader.php');
+    $ClassLoader = new \Pvik\Core\ClassLoader();
+    $ClassLoader->SetNamespaceAssociation('\\Pvik', '~/library/pvik/');
+    $ClassLoader->SetNamespaceAssociation('\\MyProject', '~/application/');
+    $ClassLoader->Init();
+    
+    //require_once ("./library/pvik/core/core.php");
+    $Core = new \Pvik\Core\Core();
+    $Core->LoadConfig()
         ->StartWeb();
-?>
