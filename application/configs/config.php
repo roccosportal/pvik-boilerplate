2b8de409f50be0c00f415fb12effa992f12f2d79
roccosportal <rocco@roccosportal.com>
2013-01-26 22:52:02 +0100

    change for pvik 5.0.0

--- application/configs/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-self::$Config['DefaultNamespace'] = '\\MyProject';
-self::$Config['NamespaceAssociations']['\\MyProject'] = '~/application/';
-
-self::$Config['Routes'] = array(
-    array ('Url' => '/', 'Controller' => 'HelloWorld', 'Action' => 'Index'),
-);
-?>
